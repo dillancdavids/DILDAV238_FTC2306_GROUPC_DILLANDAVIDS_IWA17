c510b14550f83ad71534f61d7a3f25f7a3db8054
dillancdavids <137268080+dillancdavids@users.noreply.github.com>
2023-08-30 18:29:41 +0200

    added corrections

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// scripts.js
+
 const MONTHS = [
     'January',
     'February',
@@ -15,71 +17,101 @@ const MONTHS = [
 
 const getDaysInMonth = (date) => new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
 
+// Only edit below 
+
 const createArray = (length) => {
+
     const result = [];
 
     for (let i = 0; i < length; i++) {
-        result.push(i);
+        result.push(i);     //Add value of 'i' to result array
     }
 
     return result;
-};
+}
+
 
 const createData = () => {
     const current = new Date();
     current.setDate(1);
 
-    const startDay = current.getDay(); // Day of the week (0-6) of the 1st day
+    const startDay = current.getDay();  //Returns the day of the week
     const daysInMonth = getDaysInMonth(current);
 
-    const weeks = createArray(Math.ceil((startDay + daysInMonth) / 7));
+    const weeks = createArray(5);
+    const days = createArray(7);
+    // const value = null;
     const result = [];
 
-    for (const weekIndex of weeks) {
-        result.push({
+
+
+    for (let weekIndex = 0; weekIndex < weeks.length; weekIndex++) {
+        const value = {     //Declared value with const and fixed to be an object
             week: weekIndex + 1,
             days: []
-        });
+        }
 
-        for (let dayIndex = 0; dayIndex < 7; dayIndex++) {
-            const day = dayIndex + 1 + weekIndex * 7 - startDay;
+
+        for (let dayIndex = 0; dayIndex < days.length; dayIndex++) {
+            const day = weekIndex * 7 + dayIndex - startDay + 1;    //calculate the day number of current day on calender
             const isValid = day > 0 && day <= daysInMonth;
 
-            result[weekIndex].days.push({
-                dayOfWeek: dayIndex,
-                value: isValid ? day : '',
-            });
+            value.days.unshift({
+                dayOfWeek: dayIndex + 1,
+                value: isValid ? day : ""     // day if 'isValid' is true and null if false
+            })
+            
         }
-    }
+        result.push(value);
 
+    }
     return result;
-};
+}
+
+
+
+const addCell = (existing, classString, value) => {
+    const result = /* html */ `
+        <td class=" ${classString}">
+            ${value}
+        </td>
+
+        ${existing}
+    `
+    return result;
+}
 
 const createHtml = (data) => {
     let result = '';
 
-    for (const { week, days } of data) {
+    for (let week of data) {
         let inner = "";
-        inner = addCell(inner, 'table__cell table__cell_sidebar', `Week ${week}`);
+       
 
-        for (const { dayOfWeek, value } of days) {
-            const isToday = new Date().getDate() === value;
-            const isWeekend = dayOfWeek === 0 || dayOfWeek === 6;
-            const isAlternate = week % 2 === 0;
-
+        for (let day of week.days) {
             let classString = 'table__cell';
-            if (isToday) classString += ' table__cell_today';
-            if (isWeekend) classString += ' table__cell_weekend';
-            if (isAlternate) classString += ' table__cell_alternate';
+            const isToday = new Date().getDate() === day.value;
+            const isWeekend = day.dayOfWeek === 1 || day.dayOfWeek === 7;
+            const isAlternate = week.week % 2 === 0;
 
-            inner = addCell(inner, classString, value);
+            // let classString = 'table__cell'
+
+            if (isToday) classString = `${classString} table__cell_today`;      // correct syntax for interpolation
+            if (isWeekend) classString = `${classString} table__cell_weekend`;    // correct syntax for interpolation
+            if (isAlternate) classString = `${classString} table__cell_alternate`;    // correct syntax for interpolation 
+
+            inner = addCell(inner, classString, day.value)
         }
-
-        result += `
-            <tr>${inner}</tr>
-        `;
+        inner =  addCell(inner, 'table__cell table__cell_sidebar', `Week ${week.week}`);
+        result += `<tr>${inner}</tr>`;
     }
-
     return result;
-};
+}
 
+// Only edit above
+
+const current = new Date()
+document.querySelector('[data-title]').innerText = `${MONTHS[current.getMonth()]} ${current.getFullYear()}`
+
+const data = createData()
+document.querySelector('[data-content]').innerHTML = createHtml(data)
